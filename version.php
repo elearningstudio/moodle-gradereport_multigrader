1fac681ee370467e466d709e95d33cda77d59706
Barry Oosthuizen <barry@elearningstudio.co.uk>
2013-09-14 21:56:52 +0100

    Updated version info

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2012061700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2012061700;        // Requires this Moodle version
 $plugin->component = 'gradereport_multigrader'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.9';
