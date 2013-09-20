89631ab4604ae37ab40e93c2b59458026dd02700
Barry Oosthuizen <barry@elearningstudio.co.uk>
2013-09-20 23:50:50 +0100

    Version bump (skipped 0.9.2) to 0.9.3

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013091800.1;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2013091800.3;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2013040500;        // Requires this Moodle version
 $plugin->component = 'gradereport_multigrader'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.9.2';
+$plugin->release   = '0.9.3';
